990d656c86dd8d061cc3155685ec065efee3828d
Steven Smith <ssmith563@umail.ucsb.edu>
2022-08-21 16:32:26 -0700

    outline of tsp greedy

--- tsp.c
+++ tsp.c
@@ -2,101 +2,87 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
-#define PLACES 5
 
-int main();
-char* InitiateCharArray (int);
-int* InitiateIntArray (int);
-char** InitiateMultiCharArray (int, int);
-int GetPossibleOptions (int);
-int GetRandomNumber (int);
-int GetPossibleOptions (int);
+
+void travellingSalesman(int n, int path[n], int cost[n][n], int * costSum);
+void printCostArray(int n, int arr[n][n]);
+void printPathArray(int n, int arr[n]);
 
 int main ()
 {
-    char *TravledPlaces = InitiateCharArray(PLACES);
-    char **TravelOptions = InitiateMultiCharArray(GetPossibleOptions(PLACES), PLACES+1);
-    int  *NextPlaceOptions = InitiateIntArray(PLACES-1);
-    int i, j = 0;
-    bool solved = false;
+    //will replace with arguments
+    int n = 4;
+    
+    //will replace with function call that calculates distance cost between 3d points
+    int cost[n][n];
+    cost[0][0] = 0;
+    cost[0][1] = 4;
+    cost[0][2] = 1;
+    cost[0][3] = 3;
 
-    for (i = 0; i < PLACES-1; i++) {
-        NextPlaceOptions[i] = i+1;
-    }
-    printf("NextPlaceOptions: {%d, %d, %d, %d} \n", NextPlaceOptions[0], NextPlaceOptions[1], NextPlaceOptions[2], NextPlaceOptions[3]);
+    cost[1][0] = 4;
+    cost[1][1] = 0;
+    cost[1][2] = 2;
+    cost[1][3] = 1;
 
-    // Debug
-    int FirstLocationOptions = GetPossibleOptions(PLACES)/(PLACES-1);
-    printf("First: %d \n", FirstLocationOptions);
-    int SecondLocationOptions = FirstLocationOptions/(PLACES-2);
-    printf("Second: %d \n", SecondLocationOptions);
-    int ThirdLocationOptions = SecondLocationOptions/(PLACES-3);
-    printf("Third: %d \n\n", ThirdLocationOptions);
+    cost[2][0] = 1;
+    cost[2][1] = 2;
+    cost[2][2] = 0;
+    cost[2][3] = 5;
 
-    while (!solved) {
-        int size = 0;
-        for (i = 0; i < PLACES-1; i++) {
-            if (NextPlaceOptions[i] != -1) size++;
-        }
-        GetRandomNumber(size);
-    }
+    cost[3][0] = 3;
+    cost[3][1] = 1;
+    cost[3][2] = 5;
+    cost[3][3] = 0;
 
-    int NextPlace = 1;
-    for (i = 0; i < GetPossibleOptions(PLACES); i++) {
-        TravelOptions[i][0] = 0;
-        TravelOptions[i][5] = 0;
+    
+    //prints cost array
+    printCostArray(n, cost);
 
-        TravelOptions[i][1] = NextPlace;
-        j++;
+    //final path salesman takes
+    int path[n];
+    //cost of final path
+    int costSum;
 
-        if (j == FirstLocationOptions) {
-            NextPlace += 1;
-            j = 0;
-        }
-    }
+    //main tsp function
+    travellingSalesman(n, path, cost, &costSum);
 
-    // Debug
-    for (i = 0; i < GetPossibleOptions(PLACES); i++) {
-        printf("{%d, %d, %d, %d, %d, %d} \n", TravelOptions[i][0], TravelOptions[i][1], TravelOptions[i][2],
-                                              TravelOptions[i][3], TravelOptions[i][4], TravelOptions[i][5]);
-    }
+    //print final path
+    printPathArray(n, path);
+    
+    printf("%d\n", costSum);
+    
     return 0;
+
 }
 
-char* InitiateCharArray (int size) {
-    char *TravledPlaces = (char *)malloc(size*sizeof(char));
-    return TravledPlaces;
-}
+void travellingSalesman(int n, int path[n], int cost[n][n], int * costSum){
 
-int* InitiateIntArray (int size) {
-    int *TravledPlaces = (int *)malloc(size*sizeof(int));
-    int i = 0;
-    for (i = 0; i < size; i++) {
-        TravledPlaces[i] = 0;
+    //replace with greedy alg
+    for(int i = 0; i < n; i++){
+        path[i] = 0;
     }
-    return TravledPlaces;
+
+    *costSum = 0;
 }
 
-char** InitiateMultiCharArray (int sizeY, int sizeX) {
-    char **TravelOptions = (char **)malloc(sizeY * sizeof(char*));
-    int i;
-
-    for (i = 0; i < sizeY; i++) {
-        TravelOptions[i] = (char *)malloc(sizeX * sizeof(char));
+void printCostArray(int n, int arr[n][n]){
+    printf("Two Dimensional array elements:\n");
+    for(int i=0; i<n; i++) {
+        printf("\n");
+        for(int j=0;j<n;j++) {
+            printf("%d ", arr[i][j]);
+        }
     }
-    return TravelOptions;
+    printf("\n\n");
 }
 
-// Generates random number
-int GetRandomNumber (int size) {
-    time_t TimeStore;
-    time(&TimeStore);
-    srand((unsigned int)TimeStore);
-
-    return (rand()%(size*size))+1;
-}
-
-int GetPossibleOptions (int places) {
-    return places == 1 ? 1 : (places-1)*GetPossibleOptions(places-1);
+void printPathArray(int n, int arr[n]){
+    printf("Path array elements:\n");
+    for(int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
 }
